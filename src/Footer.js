7b22620b40b012aba8a8315653f4724020e6accb
Emanueljuarez <zerauleuname@gmail.com>
2022-02-21 13:02:26 -0600

    Mi primera aplicacion

---
+++ src/Footer.js
@@ -0,0 +1,10 @@
+function Footer (props){
+    return(
+        <footer>
+            <p>Este es el footer</p>
+            <p>Señor {props.apellido}</p>
+        </footer>
+    )
+}
+
+export default Footer
